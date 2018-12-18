15583891d1517519b2566676512a994e048caf4a
chiranjeevbitm <31815150+chiranjeevbitm@users.noreply.github.com>
2018-12-18 22:42:04 +0530

    Update face_recognition.py

--- face recog/face_recognition.py
+++ face recog/face_recognition.py
@@ -1,14 +1,4 @@
-# Recognise Faces using some classification algorithm - like Logistic, KNN, SVM etc.
 
-
-# 1. load the training data (numpy arrays of all the persons)
-		# x- values are stored in the numpy arrays
-		# y-values we need to assign for each person
-# 2. Read a video stream using opencv
-# 3. extract faces out of it
-# 4. use knn to find the prediction of face (int)
-# 5. map the predicted id to name of the user 
-# 6. Display the predictions on the screen - bounding box and name
 
 import cv2
 import numpy as np 
